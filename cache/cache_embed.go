386fca8bb84a8ee670ba740ae5d0897b16a60743
Fesaa <77553571+Fesaa@users.noreply.github.com>
2024-03-13 19:20:07 +0100

    Expose JSON for custom cache's

--- cache/cache_embed.go
+++ cache/cache_embed.go
@@ -16,3 +16,23 @@ var starRailCharacterJson []byte
 
 //go:embed resources/honkai_avatars.json
 var starRailAvataJson []byte
+
+func GetGenshinNameCardJson() []byte {
+	return genshinNameCardJson
+}
+
+func GetGenshinProfileIdentifiersJson() []byte {
+	return genshinProfileIdentifiersJson
+}
+
+func GetGenshinCharactersJson() []byte {
+	return genshinCharactersJson
+}
+
+func GetStarRailCharacterJson() []byte {
+	return starRailCharacterJson
+}
+
+func GetStarRailAvatarJson() []byte {
+	return starRailAvataJson
+}
