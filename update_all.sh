ee7b4d8c0bc5fb199cdb8661068ff37cd3e306ea
Amelia <77553571+Fesaa@users.noreply.github.com>
2025-01-02 15:00:39 +0100

    Genshin 5.3
    Make script to update, changes now :(

---
+++ update_all.sh
@@ -0,0 +1,8 @@
+curl https://raw.githubusercontent.com/EnkaNetwork/API-docs/refs/heads/master/store/characters.json > data/resources/genshin_characters.json
+curl https://raw.githubusercontent.com/EnkaNetwork/API-docs/refs/heads/master/store/namecards.json > data/resources/genshin_namecards.json
+curl https://raw.githubusercontent.com/EnkaNetwork/API-docs/refs/heads/master/store/pfps.json > data/resources/genshin_profiles.json
+
+curl https://raw.githubusercontent.com/EnkaNetwork/API-docs/refs/heads/master/store/hsr/honker_avatars.json > data/resources/honkai_avatars.json
+curl https://raw.githubusercontent.com/EnkaNetwork/API-docs/refs/heads/master/store/hsr/honker_characters.json > data/resources/honkai_characters.json
+curl https://raw.githubusercontent.com/EnkaNetwork/API-docs/refs/heads/master/store/hsr/honker_weps.json > data/resources/honkai_lightcones.json
+curl https://raw.githubusercontent.com/EnkaNetwork/API-docs/refs/heads/master/store/hsr/honker_relics.json > data/resources/honkai_relics.json
