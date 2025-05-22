a7b6c762871645dc68dde5c3167f8c224f8c552f
Amelia <77553571+Fesaa@users.noreply.github.com>
2025-05-22 11:33:55 +0200

    3.3 HSR

--- update_all.sh
+++ update_all.sh
@@ -2,7 +2,7 @@ curl https://raw.githubusercontent.com/EnkaNetwork/API-docs/refs/heads/master/st
 curl https://raw.githubusercontent.com/EnkaNetwork/API-docs/refs/heads/master/store/namecards.json > data/resources/genshin_namecards.json
 curl https://raw.githubusercontent.com/EnkaNetwork/API-docs/refs/heads/master/store/pfps.json > data/resources/genshin_profiles.json
 
-curl https://raw.githubusercontent.com/EnkaNetwork/API-docs/refs/heads/master/store/hsr/honker_avatars.json > data/resources/honkai_avatars.json
-curl https://raw.githubusercontent.com/EnkaNetwork/API-docs/refs/heads/master/store/hsr/honker_characters.json > data/resources/honkai_characters.json
-curl https://raw.githubusercontent.com/EnkaNetwork/API-docs/refs/heads/master/store/hsr/honker_weps.json > data/resources/honkai_lightcones.json
-curl https://raw.githubusercontent.com/EnkaNetwork/API-docs/refs/heads/master/store/hsr/honker_relics.json > data/resources/honkai_relics.json
+curl https://raw.githubusercontent.com/pizza-studio/EnkaDBGenerator/refs/heads/main/Sources/EnkaDBFiles/Resources/Specimen/HSR/honker_avatars.json > data/resources/honkai_avatars.json
+curl https://raw.githubusercontent.com/pizza-studio/EnkaDBGenerator/refs/heads/main/Sources/EnkaDBFiles/Resources/Specimen/HSR/honker_characters.json > data/resources/honkai_characters.json
+curl https://raw.githubusercontent.com/pizza-studio/EnkaDBGenerator/refs/heads/main/Sources/EnkaDBFiles/Resources/Specimen/HSR/honker_weps.json > data/resources/honkai_lightcones.json
+curl https://raw.githubusercontent.com/pizza-studio/EnkaDBGenerator/refs/heads/main/Sources/EnkaDBFiles/Resources/Specimen/HSR/honker_relics.json > data/resources/honkai_relics.json
